1a75ed8d7eaae1806dc303f7633ce623739b63e3
abbanat <abbanatg123@gmail.com>
2020-04-13 18:08:28 -0400

    neko help!

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
-var background = new Image();
-background.src = "./images/kirby backround.png"
-background.onload = function(){
-    ctx.drawImage(background,0,0)}
+//var background = new Image();
+//background.src = "./images/kirby backround.png"
+//background.onload = function(){
+  //  ctx.drawImage(background,0,0)}
 
 
 var canvas = document.querySelector("canvas"),
 ctx = canvas.getContext("2d"),
-  width = 500,
-  height = 200,
+  width = window.innerWidth,
+  height = window.innerHeight,
   player = {
       x: width / 2,
       y: height - 5,
@@ -42,6 +42,7 @@ function update(){
   
 // check keys
 if (keys[38]) {
+    
     //up arrow
     if(!player.jumping){
         player.jumping = true;
@@ -60,6 +61,7 @@ if (keys[39]){
         if(player.velX > -player.speed) {
             player.velX--
         }
+    
     }
 
     player.velY += gravity
@@ -83,18 +85,48 @@ if (keys[39]){
 
     ctx.clearRect(0,0,width,height);
 
-    ctx.fill();
+    
     ctx.fillStyle = "red";
 
     ctx.fillRect (player.x, player.y, player.width, player.height);
+
+    drawPlatform()
+    detectCollision()
 //runs through loop again
     requestAnimationFrame(update);
 }
+let platform = {
+    x: 20,
+    y: window.innerHeight - 50,
+    height: 50,
+    width: 200,
+}
+function drawPlatform() {
+    ctx.fillStyle = "pink"
 
+    ctx.fillRect(platform.x,platform.y,platform.width,platform.height)
+
+} 
+function detectCollision(){
+
+    
+    
+    if (player.x < platform.x + platform.width &&
+       player.x + player.width > platform.x &&
+       player.y < platform.y + platform.height &&
+       player.y + player.height > platform.y) {
+        // collision detected! 
+       player.y = platform.y - player.height
+       player.velY = 0
+       
+        console.log(player, platform)
+    }
+}
 document.body.addEventListener('keydown', function(e) {
     keys[e.keyCode] = true;
 });
 document.body.addEventListener("keyup", function(e){
+    
     keys[e.keyCode] = false;
 
 })
